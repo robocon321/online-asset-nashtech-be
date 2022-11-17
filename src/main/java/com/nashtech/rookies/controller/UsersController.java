37730cb24975d875d06e6b94260c4493370a783a
KhuongDuy-Nguyen <duyntkh@gmail.com>
2022-11-17 10:55:48 +0700

    Create UsersController.java

---
+++ src/main/java/com/nashtech/rookies/controller/UsersController.java
@@ -0,0 +1,24 @@
+package com.nashtech.rookies.controller;
+
+import com.nashtech.rookies.services.interfaces.UsersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public class UsersController {
+
+    UsersService usersService;
+
+    @Autowired
+    public UsersController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
+
+    
+
+}
