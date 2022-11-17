d473719167f87ebdcb06787a39fa917379d0f270
KhuongDuy-Nguyen <duyntkh@gmail.com>
2022-11-17 10:53:25 +0700

    Create interface service user

---
+++ src/main/java/com/nashtech/rookies/services/interfaces/UsersService.java
@@ -0,0 +1,10 @@
+package com.nashtech.rookies.services.interfaces;
+
+import com.nashtech.rookies.entity.Users;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UsersService {
+
+}
