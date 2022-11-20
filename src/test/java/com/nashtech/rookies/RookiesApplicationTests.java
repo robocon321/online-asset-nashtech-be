a2990cd5ee986beed1e0be48ae21b0086988f2be
KhuongDuy-Nguyen <duyntkh@gmail.com>
2022-11-20 12:10:25 +0700

    add config for h2 database

---
+++ src/test/java/com/nashtech/rookies/RookiesApplicationTests.java
@@ -0,0 +1,10 @@
+package com.nashtech.rookies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RookiesApplicationTests {
+	@Test
+	void contextLoads() {}
+}
