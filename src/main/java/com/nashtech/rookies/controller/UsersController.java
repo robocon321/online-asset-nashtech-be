b5800430a5a25f4c23e7798b125443076ea6f7c5
Minh LE <minhlen2@nashtechglobal.com>
2022-11-21 09:56:55 +0000

    T000 Updated UsersController.java

--- src/main/java/com/nashtech/rookies/controller/UsersController.java
+++ src/main/java/com/nashtech/rookies/controller/UsersController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin("http://localhost:3000/")
+@CrossOrigin
 @RequestMapping("/api/v1/users")
 @RestController
 public class UsersController {
