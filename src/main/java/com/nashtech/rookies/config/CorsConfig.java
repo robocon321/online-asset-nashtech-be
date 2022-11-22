3c3aed48a3a188b54c247b9978aa7030e6fb6818
KhuongDuy-Nguyen <duyntkh@gmail.com>
2022-11-22 23:52:49 +0700

    Update CorsConfig.java

--- src/main/java/com/nashtech/rookies/config/CorsConfig.java
+++ src/main/java/com/nashtech/rookies/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
         return new WebMvcConfigurerAdapter() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
+                registry.addMapping("/api/v1/**")
                         .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
             }
         };
