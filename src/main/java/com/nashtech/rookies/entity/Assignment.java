0d1f7f26b7074bb10fdfa449dda00cef6f7060e4
phuhung9292 <phuhungx1810@gmail.com>
2022-11-28 15:28:01 +0700

    T1183 update entity

--- src/main/java/com/nashtech/rookies/entity/Assignment.java
+++ src/main/java/com/nashtech/rookies/entity/Assignment.java
@@ -18,9 +18,9 @@ public class Assignment {
 
     private String note;
     private String state;
-    private Date assigned_date;
-    private Date returned_date;
-    private Date created_date;
+    private Date assignedDate;
+    private Date returnedDate;
+    private Date createdDate;
 
     @ManyToOne
     private Users assignedBy;
@@ -31,14 +31,14 @@ public class Assignment {
     @ManyToOne
     private Asset asset;
 
-    public Assignment(String note, String state, Date assigned_date, Date returned_date, Date created_date, Users assigned_by, Users assigned_to, Asset asset) {
+    public Assignment(String note, String state, Date assignedDate, Date returnedDate, Date createdDate, Users assignedBy, Users assignedTo, Asset asset) {
         this.note = note;
         this.state = state;
-        this.assigned_date = assigned_date;
-        this.returned_date = returned_date;
-        this.created_date = created_date;
-        this.assignedBy = assigned_by;
-        this.assignedTo = assigned_to;
+        this.assignedDate = assignedDate;
+        this.returnedDate = returnedDate;
+        this.createdDate = createdDate;
+        this.assignedBy = assignedBy;
+        this.assignedTo = assignedTo;
         this.asset = asset;
     }
 }
