792c8e2cb3f266cf4058f61776e11051d8d18b5b
phuhung9292 <phuhungx1810@gmail.com>
2022-11-28 11:44:31 +0700

    T1183 resolve conflict

--- src/main/java/com/nashtech/rookies/repository/AssignmentRepository.java
+++ src/main/java/com/nashtech/rookies/repository/AssignmentRepository.java
@@ -2,15 +2,11 @@ package com.nashtech.rookies.repository;
 
 import com.nashtech.rookies.entity.Asset;
 import com.nashtech.rookies.entity.Assignment;
-<<<<<<< HEAD
 import com.nashtech.rookies.entity.Users;
-=======
 
 import java.util.List;
 
->>>>>>> main
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
