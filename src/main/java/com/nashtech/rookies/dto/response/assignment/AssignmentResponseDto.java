19fe352092aa64e657a30b2c5448e2dee1adadd6
phuhung9292 <phuhungx1810@gmail.com>
2022-12-04 19:04:55 +0700

    update code

--- src/main/java/com/nashtech/rookies/dto/response/assignment/AssignmentResponseDto.java
+++ src/main/java/com/nashtech/rookies/dto/response/assignment/AssignmentResponseDto.java
@@ -17,14 +17,10 @@ public class AssignmentResponseDto {
 	private Long id;
 	private String assetCode;
 	private String assetName;
-
 	private String categoryName;
-
 	private String assignedTo;
 	private String assignedBy;
 	private Date assignedDate;
-
 	private boolean stateReturnRequest;
-
 	private String state;
 }
